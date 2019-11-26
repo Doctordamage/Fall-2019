afeee4ccf44223e0c0ac85ccbc5ef37595e67e1c
Doctordamage <44416533+Doctordamage@users.noreply.github.com>
2019-11-26 14:16:05 -0700

    Update Lab 5 CS2302 Fall 2019.py

--- CS2302 Labs/Lab 5/Lab 5 CS2302 Fall 2019.py
+++ CS2302 Labs/Lab 5/Lab 5 CS2302 Fall 2019.py
@@ -1,3 +1,6 @@
+# Course:CS 2302 MW 1:30-2:50, Author:David Ayala
+# Assignment:Lab #1, Instructor: Diego Aguirre
+# Teaching Assistant: ????, Date of last Modification: 9/10/2019
 import time
 import PartA as lru
 from PartA import *
